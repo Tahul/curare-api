4ae59bb7bf380bd6be072110f9d47c10c0365e9b
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-03 03:56:12 +0100

    :sparkles: (opengraph) improve preview & parser method

---
+++ app/Helpers/OpenGraph/Parser.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Helpers\OpenGraph {
+
+    use DOMDocument;
+
+    class Parser {
+        /**
+         * Parse content into an array.
+         * @param $content mixed
+         * @return array
+         */
+        public static function parse($content) {
+            $doc = new DOMDocument();
+
+            // Fudge to handle a situation when an encoding isn't present
+            if (strpos($content, 'xml encoding=')===false)
+                $content = '<?xml encoding="utf-8" ?>' . $content;
+
+            @$doc->loadHTML($content);
+
+            $interested_in = ['description', 'keywords', 'og', 'fb', 'twitter']; // Open graph namespaces we're interested in (open graph + extensions)
+
+            $ogp = [];
+
+            // Title
+            $titleTag = $doc->getElementsByTagName('title')->item(0);
+            if (!is_null($titleTag)) {
+                $ogp['title'] = $titleTag->nodeValue;
+            }
+
+            // Open graph
+            $metas = $doc->getElementsByTagName('meta');
+            if (!empty($metas)) {
+                foreach ($metas as $meta) {
+                    foreach (['name', 'property'] as $name) {
+                        $attribute = $meta->getAttribute($name);
+                        $meta_bits = explode(':', $attribute);
+                        if (in_array($meta_bits[0], $interested_in)) {
+                            // If we're adding to an existing element, convert it to an array
+                            if (isset($ogp[$attribute]) && (!is_array($ogp[$attribute])))
+                                $ogp[$meta_bits[0]][$attribute] = array($ogp[$attribute], $meta->getAttribute('content'));
+                            else if (isset($ogp[$attribute]) && (is_array($ogp[$attribute])))
+                                $ogp[$meta_bits[0]][$attribute][] = $meta->getAttribute('content');
+                            else
+                                $ogp[$meta_bits[0]][$attribute] = $meta->getAttribute('content');
+                        }
+                    }
+                }
+            }
+
+            // OEmbed
+            $metas = $doc->getElementsByTagName('link');
+            if (!empty($metas)) {
+                foreach ($metas as $meta) {
+                    if (strtolower($meta->getAttribute('rel')) == 'alternate') {
+
+                        if (in_array(strtolower($meta->getAttribute('type')), ['application/json+oembed'])) {
+                            $ogp['oembed']['jsonp'][] = $meta->getAttribute('href');
+                        }
+                        if (in_array(strtolower($meta->getAttribute('type')), ['text/json+oembed'])) {
+                            $ogp['oembed']['json'][] = $meta->getAttribute('href');
+                        }
+                        if (in_array(strtolower($meta->getAttribute('type')), ['text/xml+oembed'])) {
+                            $ogp['oembed']['xml'][] = $meta->getAttribute('href');
+                        }
+                    }
+                }
+            }
+
+            // Basics
+            foreach (['title'] as $basic) {
+                if (preg_match("#<$basic>(.*?)</$basic>#siu", $content, $matches))
+                    $ogp[$basic] = trim($matches[1], " \n");
+            }
+
+            $metas = $doc->getElementsByTagName('meta');
+
+            if (!empty($metas)) {
+                foreach ($metas as $meta) {
+                    if (strtolower($meta->getAttribute('name')) == 'description') {
+                        $ogp['description'] = $meta->getAttribute('content');
+                    }
+                    if (strtolower($meta->getAttribute('name')) == 'keywords') {
+                        $ogp['keywords'] = $meta->getAttribute('content');
+                    }
+                }
+            }
+
+            return $ogp;
+        }
+    }
+}
--- app/Http/Controllers/OpenGraph/OpenGraphController.php
+++ app/Http/Controllers/OpenGraph/OpenGraphController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\OpenGraph;
 
+use App\Helpers\OpenGraph\Parser;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Link\LinkPreviewRequest;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use ogp\Parser;
 
 class OpenGraphController extends Controller
 {
@@ -18,7 +18,8 @@ class OpenGraphController extends Controller
      */
     public function preview(LinkPreviewRequest $request)
     {
-        $data = null;
+        $data = [];
+        $domain = parse_url($request->url)["host"];
 
         try {
             $content = file_get_contents($request->url);
@@ -26,10 +27,17 @@ class OpenGraphController extends Controller
             $data = Parser::parse($content);
         } catch (Exception $e) {
             // Neglect this case, wrong URL.
+            info($e);
         }
 
         return response()->json(
-            $data
+            array_merge(
+                [
+                    'domain' => $domain,
+                    'favicon' => 'https://www.google.com/s2/favicons?domain=' . $domain
+                ],
+                $data
+            )
         );
     }
 }
