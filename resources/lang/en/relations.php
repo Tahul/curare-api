85585e22c51a786ce775524ddbe381a94ff79923
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-08 19:38:36 +0100

    :sparkles: (relations) lang
    implement relations language file

---
+++ resources/lang/en/relations.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relations Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during relations for various
+    | messages that we need to display to the user.
+    |
+    */
+
+    'followed' => 'You are now following @:name.',
+    'unfollowed' => 'You are no longer following @:name.',
+    'error' => 'An error occured while updating your relations.',
+    'not_found' => 'Cannot find this user.',
+];
