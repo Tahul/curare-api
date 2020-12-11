745213ee567839821105d4d6d64980599b7bdc8f
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-11 16:10:39 +0100

    :sparkles: (seeders) database seeder seeding complete app data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Collection\Collection;
+use App\Models\Link\Link;
+use App\Models\User\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Seed users, collections, links
+        User::factory(10)
+            ->has(
+                Collection::factory()->count(3)
+                    ->state(function (array $attributes, User $user) {
+                        return [
+                            'user_id' => $user->id
+                        ];
+                    })
+                    ->has(
+                        Link::factory()->count(10)->state(
+                            function (array $attributes, Collection $collection) {
+                                return [
+                                    'user_id' => $collection->user_id,
+                                    'collection_id' => $collection->id
+                                ];
+                            }
+                        )
+                    )
+            )
+            ->create();
+
+        // Seed user relations
+        $users = User::all();
+
+        $users->each(
+            function(User $user) use ($users) {
+                $user->followings()->attach(
+                    $users->whereNotIn('id', [$user->id])->random(5)->pluck('id')
+                );
+            }
+        );
     }
 }
