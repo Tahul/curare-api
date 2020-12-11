13e365ab14ef13642ce43bf0af34e0101686db8d
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-11 16:09:03 +0100

    :sparkles: (seeders) collection factory

---
+++ database/factories/Collection/CollectionFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories\Collection;
+
+use App\Models\Collection\Collection;
+use App\Models\User\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CollectionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Collection::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->word,
+        ];
+    }
+}
