b6a00902222b5c002f693d9ee2267fd15aa47894
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-16 19:30:04 +0100

    :seedling: (profile) add profile seeding to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Collection\Collection;
 use App\Models\Link\Link;
+use App\Models\Profile\Profile;
 use App\Models\User\User;
 use Illuminate\Database\Seeder;
 
@@ -16,17 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::flushEventListeners();;
+
         // Seed users, collections, links
         User::factory(10)
             ->has(
-                Collection::factory()->count(3)
+                Collection::factory()->count(2)
                     ->state(function (array $attributes, User $user) {
                         return [
                             'user_id' => $user->id
                         ];
                     })
                     ->has(
-                        Link::factory()->count(10)->state(
+                        Link::factory()->count(3)->state(
                             function (array $attributes, Collection $collection) {
                                 return [
                                     'user_id' => $collection->user_id,
@@ -35,6 +38,14 @@ class DatabaseSeeder extends Seeder
                             }
                         )
                     )
+            )
+            ->has(
+                Profile::factory()
+                    ->state(function (array $attributes, User $user) {
+                        return [
+                            'user_id' => $user->id
+                        ];
+                    })
             )
             ->create();
 
@@ -46,7 +57,22 @@ class DatabaseSeeder extends Seeder
                 $user->followings()->attach(
                     $users->whereNotIn('id', [$user->id])->random(5)->pluck('id')
                 );
+
+                $user->profile->addMedia($this->getRandomImage($user->id))->preservingOriginal()->toMediaCollection('avatars');
             }
         );
+    }
+
+    /**
+     * Return a random seeded image path.
+     *
+     * @param int|null $id
+     * @return string
+     */
+    private function getRandomImage($id = null): string
+    {
+        $avatarId = is_null($id) || $id > 10 ? rand(1, 10) : $id;
+
+        return resource_path('seeders/avatar_' . $avatarId . '.jpg');
     }
 }
