054f5313da019bbd8e60f56b553457e2e29a362e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-18 16:05:31 +0100

    :wrench: (services) add twitter service to config/services

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' => env('TWITTER_REDIRECT_URI')
+    ],
 ];
